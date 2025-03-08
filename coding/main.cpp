bb1760fc079d7a699ecc81073297563e09710bfd
Hong Seongjun <129278491+Junepluson@users.noreply.github.com>
2025-03-08 16:20:35 +0900

    Initial Commit

---
+++ coding/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  coding
+//
+//  Created by seongjun Hong on 3/8/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
